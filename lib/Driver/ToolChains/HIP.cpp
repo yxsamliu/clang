2e43ac0ac33130354a95a9f0a83099c3277bb24b
Yaxun Sam Liu <yaxun.liu@amd.com>
2018-10-03 12:43:07 -0400

    Set non-kernel functions visibility to be invisible
    Set kernel linkage to external

--- lib/Driver/ToolChains/HIP.cpp
+++ lib/Driver/ToolChains/HIP.cpp
@@ -284,12 +284,6 @@ void HIPToolChain::addClangTargetOptions(
   if (DriverArgs.hasFlag(options::OPT_fgpu_rdc, options::OPT_fno_gpu_rdc,
                          false))
     CC1Args.push_back("-fgpu-rdc");
-
-  // Default to "hidden" visibility, as object level linking will not be
-  // supported for the foreseeable future.
-  if (!DriverArgs.hasArg(options::OPT_fvisibility_EQ,
-                         options::OPT_fvisibility_ms_compat))
-    CC1Args.append({"-fvisibility", "hidden"});
 }
 
 llvm::opt::DerivedArgList *
